a88a748c963da2a5bf8cc86b626b2d53e1eb7de8
manuelalm <132892406+manuelalm@users.noreply.github.com>
2023-05-29 16:44:09 +0100

    login

---
+++ Projeto Automação DEAPC/scripts/index.js
@@ -0,0 +1,10 @@
+const mostrarSenhaCheckbox = document.getElementById('mostrarSenha');
+const senhaInput = document.getElementById('senha');
+
+mostrarSenhaCheckbox.addEventListener('change', function() {
+  if (mostrarSenhaCheckbox.checked) {
+    senhaInput.type = 'text';
+  } else {
+    senhaInput.type = 'password';
+  }
+});
