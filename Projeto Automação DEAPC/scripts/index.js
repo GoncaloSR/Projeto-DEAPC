8b09a593f2c2cb7f18c78cf3cd3d281ed41a2661
Emanuel Viana <e.t.viana2000@gmail.com>
2023-05-30 10:09:13 +0100

    redirect do login da pagina principal

---
+++ Projeto Automação DEAPC/scripts/index.js
@@ -0,0 +1,8 @@
+function redirectToLogin() {
+    //alert("Hello World")
+    window.location.href = "login.html";
+}
+
+function redirectToRegister() {
+    window.location.href = "register.html";
+}
