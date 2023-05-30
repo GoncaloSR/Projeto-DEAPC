e9fe88c2d662481125dd6d8ffe6a78036522af12
GoncaloSR <121798965+GoncaloSR@users.noreply.github.com>
2023-05-30 09:57:13 +0100

    pass atualizada

--- Projeto Automação DEAPC/scripts/register.js
+++ Projeto Automação DEAPC/scripts/register.js
@@ -9,20 +9,28 @@ passwordInput.addEventListener('input', function() {
 });
 
 function checkPasswordStrength(password) {
-  if (password.length < 8) {
-    return 'Fraca';
+  
+  var forcaMinima = 8; // Tamanho mínimo da senha
+  var caracteresEspeciais = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/; // Pelo menos um caractere especial
+  var numeros = /[0-9]/; // Pelo menos um número
+  
+  if (password.length < forcaMinima) {
+    return "A senha deve ter no mínimo " + forcaMinima + " caracteres.";
   }
   
-  if (/[a-zA-Z]/.test(password) && /[0-9]/.test(password) && /[^a-zA-Z0-9]/.test(password)) {
-    return 'Forte';
+  if (!caracteresEspeciais.test(senha)) {
+    return "A senha deve conter pelo menos um caractere especial.";
   }
-  if (/[a-zA-Z]/.test(password)) {
-  return 'Média';
+  if (!numeros.test(senha)) {
+    return "A senha deve conter pelo menos um número.";
   }
-  if (/[0-9]/.test(password)) {
-    return 'Média';
-  }
-  if (password.length > 8) {
-    return 'Media';
-  }
+  return "A senha é forte.";
+
+
+  //if (/[0-9]/.test(password)) {
+    //return 'Média';
+  //}
+  //if (password.length > 8) {
+    //return 'Media';
+  //}
 }
