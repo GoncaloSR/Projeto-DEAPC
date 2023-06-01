9d9683eb63d88bd3b8760d3320a18b265d6f69c8
manuelalm <132892406+manuelalm@users.noreply.github.com>
2023-06-01 09:47:47 +0100

    alert

--- Projeto Automação DEAPC/scripts/login.js
+++ Projeto Automação DEAPC/scripts/login.js
@@ -9,3 +9,31 @@ mostrarSenhaCheckbox.addEventListener('change', function() {
   }
 }); 
 
+
+/*
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  var username = document.getElementsByName("username")[0].value;
+  var password = document.getElementsByName("password")[0].value;
+  
+  // Verifique as credenciais aqui (isso é apenas um exemplo)
+  if (username === "admin" && password === "password") {
+    // Credenciais válidas, permita o login
+    alert("Login bem-sucedido!");
+  } else {
+    // Credenciais inválidas, exiba o alerta
+    event.preventDefault(); // Impede o envio do formulário
+    
+    var alertDiv = document.createElement("div");
+    alertDiv.classList.add("alert");
+    alertDiv.innerText = "Credenciais inválidas!";
+    
+    document.body.appendChild(alertDiv);
+    setTimeout(function() {
+      alertDiv.style.display = "none";
+    }, 3000); // Exibe o alerta por 3 segundos
+  }
+});
+
+****JS para alerta caso credenciais forem as erradas
+
+*/ 
