d62cb2097fe8c5de24a1d584901c160633c5e5be
manuelalm <132892406+manuelalm@users.noreply.github.com>
2023-06-06 23:37:55 +0100

    Introdução de um JS para mostrar informação

---
+++ Projeto Automação DEAPC/scripts/produto.js
@@ -0,0 +1,13 @@
+/*function showInfo(info) {
+    var infoContainer = document.getElementById('infoContainer');
+    var infoTextbox = document.getElementById('infoTextbox');
+    infoTextbox.value = info;
+    infoContainer.style.display = 'block';
+  }
+  
+  function hideInfo() {
+    var infoContainer = document.getElementById('infoContainer');
+    infoContainer.style.display = 'none';
+  }
+
+  */
