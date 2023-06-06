828ed2a7dc9250b6e668be5123d600eac88a2319
manuelalm <132892406+manuelalm@users.noreply.github.com>
2023-06-06 09:58:52 +0100

    carpet

--- Projeto Automação DEAPC/scripts/login.js
+++ Projeto Automação DEAPC/scripts/login.js
@@ -9,3 +9,18 @@ mostrarSenhaCheckbox.addEventListener('change', function() {
   }
 }); 
 
+function validateForm() {
+  var nome = document.getElementById("nome").value;
+  var senha = document.getElementById("senha").value;
+
+  // Realize a validação das credenciais aqui
+  // Por exemplo, você pode verificar se os campos estão preenchidos corretamente
+
+  if (nome === "" || senha === "") {
+      alert("Por favor, preencha todos os campos");
+      return false; // Impede o envio do formulário se algum campo estiver vazio
+  }
+
+  // Se a validação for bem-sucedida, o formulário será enviado
+  return true;
+}
