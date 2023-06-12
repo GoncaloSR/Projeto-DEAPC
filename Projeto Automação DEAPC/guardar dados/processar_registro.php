a56ddb84ce5fa1c672139624d50e109e7bc28284
Emanuel Viana <e.t.viana2000@gmail.com>
2023-06-12 20:27:18 +0100

    registro e login

--- Projeto Automação DEAPC/guardar dados/processar_registro.php
+++ Projeto Automação DEAPC/guardar dados/processar_registro.php
@@ -1,46 +1,54 @@
-
 <?php
-// Recebe os dados do formulário
-/*
+// Recebe os dados do formulário de registro
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-// Aqui pode-se adicionar a lógica para armazenar os dados em um banco de dados ou fazer qualquer outra ação necessária
+// Conectar ao banco de dados SQLite
+$db = new SQLite3('entrada.db');
 
-// Exemplo de exibição dos dados cadastrados
-echo "Nome: " . $nome . "<br>";
-echo "Email: " . $email . "<br>";
-echo "Senha: " . $senha . "<br>";
-?>*/
-//...
-// Obter os valores enviados pelo formulário
-$nome = $_POST['nome'];
-$senha = $_POST['senha'];
-$email = $_POST['email'];
-
-// Conectar ao banco de dados
-$conexao = mysqli_connect('http://localhost/Projeto-DEAPC/register.html', 'root', '', 'bancoregisto');
-
-// Verificar se a conexão foi estabelecida corretamente
-if (!$conexao) {
-    die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
+// Verifica se a conexão foi estabelecida corretamente
+if (!$db) {
+    die("Erro ao conectar ao banco de dados: " . $db->lastErrorMsg());
 }
 
-// Inserir os dados na tabela
-$query = "INSERT INTO usuarios (nome, senha, email) VALUES ('$nome', '$senha', '$email')";
-$resultado = mysqli_query($conexao, $query);
+// Inserir os dados na tabela de registro
+$query = "INSERT INTO Entradas (nome, senha, email) VALUES ('$nome', '$senha', '$email')";
+$resultado = $db->exec($query);
 
 // Verificar se o registro foi inserido com sucesso
-if ($resultado) {
-    echo "Registro realizado com sucesso!";
-} else {
-    echo "Erro ao registrar o usuário: " . mysqli_error($conexao);
+if (!$resultado) {
+    echo "Erro ao registrar o usuário: " . $db->lastErrorMsg();
+    exit;
+}
+
+// Verifica se o formulário de login foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtém os valores do formulário de login
+    $nomeLogin = $_POST["nome"];
+    $senhaLogin = $_POST["senha"];
+
+    // Verifica as credenciais no banco de dados
+    $queryLogin = "SELECT * FROM Entradas WHERE email = :nome AND senha = :senha";
+    $stmt = $db->prepare($queryLogin);
+    $stmt->bindValue(':nome', $nomeLogin);
+    $stmt->bindValue(':senha', $senhaLogin);
+    $result = $stmt->execute();
+
+    // Verifica se as credenciais são válidas
+    if ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+        // Credenciais válidas, redireciona para outra página
+        header("Location: produto.html");
+        exit;
+    } else {
+        // Credenciais inválidas, redireciona de volta para a página de login com uma mensagem de erro
+        header("Location: login.html?erro=1");
+        exit;
+    }
 }
 
 // Fechar a conexão com o banco de dados
-mysqli_close($conexao);
+$db->close();
 ?>
-
 
 
