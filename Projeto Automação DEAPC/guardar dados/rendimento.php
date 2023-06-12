96de8e39296329559e02d975bfaa9090cd01b441
manuelalm <manuelalmeida2004@gmail.com>
2023-06-12 17:40:00 +0100

    php

--- Projeto Automação DEAPC/guardar dados/rendimento.php
+++ Projeto Automação DEAPC/guardar dados/rendimento.php
@@ -4,9 +4,17 @@ $listaRendimento = [
   ['setor' => 'Setor 1', 'rendimento' => 100],
   ['setor' => 'Setor 2', 'rendimento' => 90],
   ['setor' => 'Setor 3', 'rendimento' => 95],
-  ['setor' => 'Setor 4', 'rendimento' => 80]
+  ['setor' => 'Setor 4', 'rendimento' => 80],
 ];
+
+$listaErros = [
+  ['setor' => 'setor 1', 'erros' => 21],
+  ['setor' => 'setor 2', 'erros' => 16],
+  ['setor' => 'setor 3', 'erros' => 18],
+  ['setor' => 'setor 4', 'erros' => 12],
+]
 
 // Retornar os dados como JSON
 echo json_encode($listaRendimento);
+echo json_encode($listaErros);
 ?>
